cca3a933bc3c0612a30d978527774c706370f530
LazyHacker <github@lazyhacker.com>
2020-08-06 15:42:47 -0700

    Check different OS for the different file extension type. Allow specifying a specific version to download.

--- getgo.go
+++ getgo.go
@@ -24,6 +24,7 @@ const (
 var (
 	sha_extension = ".sha256"
 	dl            = flag.String("dir", "", "Directory path to download to.")
+	version       = *flag.String("version", "", "Specific version to download (e.g. 1.14.7)")
 )
 
 func main() {
@@ -31,16 +32,36 @@ func main() {
 	// Get the OS and architecture
 	goos := runtime.GOOS
 	arch := runtime.GOARCH
+
 	resp, err := http.Get(STABLE_VERSION)
-	if err != nil {
-		log.Fatalf("Unable to get the latest version number. %v", err)
+	if version == "" {
+		if err != nil {
+			log.Fatalf("Unable to get the latest version number. %v", err)
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalf("Unable to read the version number. %v", err)
+		}
+		version = string(body)
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	version := string(body)
+
+	var gofile string
 
 	// Construct the file name for the stable binary.
-	gofile := fmt.Sprintf("%v.%v-%v.tar.gz", version, goos, arch)
+	switch goos {
+	case "linux":
+		gofile = fmt.Sprintf("%v.%v-%v.tar.gz", version, goos, arch)
+	case "freebsd":
+		gofile = fmt.Sprintf("%v.%v-%v.tar.gz", version, goos, arch)
+	case "windows":
+		gofile = fmt.Sprintf("%v.%v-%v.msi", version, goos, arch)
+	case "darwin":
+		gofile = fmt.Sprintf("%v.%v-%v.pkg", version, goos, arch)
+	default:
+		log.Fatalln("Unknown OS... can't download.")
+
+	}
 
 	// Get the checksum value from the checksum file.
 	resp, err = http.Get(fmt.Sprintf("%v/%v", GO_DOWNLOAD_URL, gofile+sha_extension))
