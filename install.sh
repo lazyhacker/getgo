74de94bfdf907eb37a70200713e6a4aa09d8e0b1
LazyHacker <github@lazyhacker.com>
2020-08-06 18:30:53 -0700

    made it a bash script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
-
-if [[ $UID !=0 ]]; then
-    echo "Please run this script with sudo:"
-    echo "sudo $0 $*"
-    exit 1
-fi
-
-dl=`goget -dir ~/Downloads -show true`
-
-rm -rf /usr/local/go
-tar -C /usr/local $dl
+#!/bin/bash                                                                     
+                                                                                
+#if [ $UID != 0 ]; then                                                         
+#    echo "Please run this script with sudo:"                                   
+#    echo "sudo $0 $*"                                                          
+#    exit 1                                                                     
+#fi                                                                             
+                                                                                
+dl=`getgo -dir ~/Downloads -show true`                                          
+                                                                                
+sudo rm -rf /usr/local/go                                                       
+sudo tar -C /usr/local -xzf $dl
